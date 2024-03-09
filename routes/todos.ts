c8333336823091bca56466d3a285df8799115c68
kaps02 <kapil02rahate@gmail.com>
2024-03-09 10:28:39 +0530

    add strict type for param and body

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,10 +1,12 @@
 import {Router} from "express";
 import {Todo} from "../models/todos";
+import bodyParser from "body-parser";
 
 var  todos : Todo [] = [];
 
 const router = Router();
-
+type reqBody = { text: string};
+type paramsBody = {todoId : string};
 
 router.get('/' , (req ,res ,next)=>{
     res.status(200).json({todos : todos});
@@ -21,21 +23,30 @@ res.status(200).json({success : true ,  todos : newTodo})
 })
 
 router.put('/todos/:todoId' , (req ,res)=>{
-const tid = req.params.todoId;
+    
+const params = req.params as paramsBody
+const tid = params.todoId;
+const body = req.body as reqBody
 
 const todoIndex = todos.findIndex((todoItm) => todoItm.id === tid);
 if(todoIndex >= 0){
-    todos[todoIndex]  = {id:todos[todoIndex].id , text : req.body.text};
+    todos[todoIndex]  = {id:todos[todoIndex].id , text : body.text};
     return res.status(200).json({success:true});
 }
 res.status(404).json({success : true});
 });
-
-router.delete('todo/:todoId' , (req ,res)=>{
+try{
+    
+router.delete('/todos/:todoId' , (req ,res)=>{
+    console.log(req.params.todoId);
     todos = todos.filter((todoItm)=>todoItm.id !== req.params.todoId);
+
     res.status(200).json({message:"deleted successfully"});
 
 })
+}catch(err){
+    console.log('error' , err)
+}
 
 export default router
 
